1fd5b49712f65c51e040e7c949bfeeb4edefa461
RodrigoE <esperfotografia94@gmail.com>
2024-05-18 14:47:36 -0300

    cambios en el titulo del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
-import '../css/Header.css'
-import logo2 from '../assets/logo2.png'
-import icono from '../assets/icono.svg'
+import "../css/Header.css";
+import logo2 from "../assets/logo2.png";
+import icono from "../assets/icono.svg";
 const Header = () => {
   return (
     <div>
-    <header>
-        <img className='logo' src={logo2} width={110} height={100}/>
-        <h1>Portfolio</h1>
-        <img className='iconodev' src={icono} width={80} />
-    </header>
+      <header>
+        <div className="logo">
+          <img className="logo" src={logo2} width={110} height={100} />
+        </div>
+        <div className="datosHeader">
+          <h3>UTN - TECNICATURA UNIVERSITARIA EN PROGRAMACION</h3>
+          <br />
+          <h4>Esper Rodrigo Fernando</h4>
+        </div>
+        <div className="iconoHeader">
+          <img className="iconodev" src={icono} width={80} />
+        </div>
+      </header>
       <hr className="linea" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
