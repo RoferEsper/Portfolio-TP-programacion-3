13d7d792e62373a3349b4728ec39b49e9fe9715e
RodrigoE <esperfotografia94@gmail.com>
2024-05-17 23:44:06 -0300

    logo icono y titulo creados en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import '../css/Header.css'
-
-
+import logo2 from '../images/logo2.png'
+import icono from '../images/icono.svg'
 const Header = () => {
   return (
     <div>
     <header>
-      
+        <img className='logo' src={logo2} width={110} height={100}/>
+        <h1>Portfolio</h1>
+        <img className='iconodev' src={icono} width={80} />
     </header>
       <hr className="linea" />
     </div>
