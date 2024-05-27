93a472e22254dcb23aa6f4a664292a99dc6b42a3
RodrigoE <esperfotografia94@gmail.com>
2024-05-26 23:28:00 -0300

    footer finalizado, se agregaron redes sociales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,19 @@
 import "../css/Footer.css"
-
+import facebook from "../assets/facebook.svg";
+import github from "../assets/github.svg"
+import instagram from "../assets/instagram.svg"
+import linkedin from "../assets/linkedin.svg"
 const Footer = () => {
   return (
     <div>
       <hr />
     <div className="ContenedorFooter">
        <div className="ContenedorRedes">
-          <div className="redes"></div>
-          <div className="redes"></div>
-          <div className="redes"></div>
-          <div className="redes"></div>
+          <div className="redes"><a href="https://www.facebook.com/Rofer94" target="_blank" rel="noopener noreferrer"><img src={facebook} alt="" width={80}/></a></div>
+          <div className="redes"><a href="https://github.com/RoferEsper" target="_blank" rel="noopener noreferrer"><img src={github} alt="" width={80}/></a></div>
+          <div className="redes"><a href="https://www.instagram.com/rodrigoesper94/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="" width={80}/></a></div>
+          <div className="redes"><a href="https://www.linkedin.com/in/rodrigo-esper-dev/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" width={80}/></a></div>
+         
        </div>
     </div>
     <hr />
